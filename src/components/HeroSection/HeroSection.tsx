9e948906fad97d823695df7a77ade018005d2ff5
themosthigh1 <dickemmanuel@gmail.com>
2023-09-05 23:50:27 -0400

    Fixed Home Page escaped chars.

--- src/components/HeroSection/HeroSection.tsx
+++ src/components/HeroSection/HeroSection.tsx
@@ -23,7 +23,7 @@ const HeroSection: FC<{ showLink?: boolean }> = (props) => {
         Candles By Sarah B.
       </h1>
       <p className="text-lg sm:text-xl md:text-2xl mb-8 text-yellow-300">
-        "{heroText}"
+        {heroText}
       </p>
       <button className="bg-black flex  hover:bg-yellow-300 text-white hover:text-black py-2 px-4">
 					Products
