68f29953c5001fbb02aa4e010b32514a12fae181
themosthigh1 <dickemmanuel@gmail.com>
2023-09-08 03:45:33 -0400

    Updated Metadata Stripe API x10

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -26,7 +26,7 @@ const Cart: FC = () => {
 	const checkoutHandler = async () => {
 		const stripe = await getStripe();
 
-		const { data } = await axios.post('https://candlesbysarahb.com/api/stripe/sessions', {
+		const { data } = await axios.post('https://candlesbysarahb.com/api/stripe', {
 			cartItems,
 			userEmail: session?.user?.email,
 		});
