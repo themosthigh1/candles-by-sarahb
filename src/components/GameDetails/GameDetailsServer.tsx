f5a0b4e27316b9f6e1851517c22145fc2b9601d0
themosthigh1 <dickemmanuel@gmail.com>
2023-09-13 12:13:12 -0400

    Added Slideshow Photo

--- src/components/GameDetails/GameDetailsServer.tsx
+++ src/components/GameDetails/GameDetailsServer.tsx
@@ -12,7 +12,7 @@ const GameDetailsServer: any = async (props: { slug: string }) => {
       <h2 className={classNames.name}>{gameDetails.name}</h2>
       <p className={classNames.price}>{gameDetails.price} $</p>
       <h2 className={classNames.description}>{gameDetails.description}</h2>
-      <h2 className={classNames.description}>{gameDetails.dimension.weightPerVessel}</h2>
+      <h2 className={classNames.dimension}>{gameDetails.dimension.weightPerVessel}</h2>
       <Link href={gameDetails.buyLink}>
         <button className="text-white mr-4 hover:bg-yellow-300 hover:text-black bg-black py-2 px-4 mt-5">
           Buy Now
@@ -26,6 +26,7 @@ export default GameDetailsServer;
 
 const classNames = {
   description: "text-lg text-black mb-2",
+  dimension: "text-lg text-black mb-2 font-medium", 
   name: "text-4xl text-black mb-2",
-  price: "text-2xl text-blackblack my-5",
+  price: "text-2xl text-black my-5",
 };
