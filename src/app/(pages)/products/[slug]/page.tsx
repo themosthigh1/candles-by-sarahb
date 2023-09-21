ca7712c896d7dcb8fd036e2a3bbc9d5b9aa45a5f
themosthigh1 <dickemmanuel@gmail.com>
2023-09-20 23:47:47 -0400

    Made addtonal products component.

--- src/app/(pages)/products/[slug]/page.tsx
+++ src/app/(pages)/products/[slug]/page.tsx
@@ -2,8 +2,15 @@
 
 import GameDetailsClient from "@/components/GameDetails/GameDetailsClient";
 import GameDetailsServer from "@/components/GameDetails/GameDetailsServer";
+import SimilarProductCard from "@/components/SimilarProductCard/SimilarProductCard";
+import { getGames } from "@/libs/apis";
+import Link from "next/link";
+import Image from "next/image";
 
 const GameItem = async (props: { params: { slug: string } }) => {
+  const games = await getGames();
+  const isFeaturedGame = games?.find((game) => game.isFeatured);
+
   const {
     params: { slug },
   } = props;
@@ -13,10 +20,22 @@ const GameItem = async (props: { params: { slug: string } }) => {
       <GameDetailsClient slug={slug}>
         <GameDetailsServer slug={slug} />
       </GameDetailsClient>
-      
+      {isFeaturedGame && (
+        <div className="mb-6">
+          <h3 className="text-2xl max-w-3xl text-center mx-auto text-primary-dark pt-12 sm:pt-28 pb-8 sm:pb-16 leading-[125%] sm:leading-[187%]">
+          SimilarProducts
+          </h3>
+          <SimilarProductCard 
+            key={isFeaturedGame._id} 
+            productName={isFeaturedGame.name} 
+            imageUrl={isFeaturedGame.images[0].url} 
+            slug={isFeaturedGame.slug.current} 
+            price={isFeaturedGame.price} 
+            buyLink={isFeaturedGame.buyLink}
+          />
+        </div>
+      )}      
     </>
-    
-    
   );
 };
 
---
+++ src/components/SimilarProductCard/SimilarProductCard.tsx
@@ -0,0 +1,48 @@
+import { FC } from "react";
+import Link from "next/link";
+import Image from "next/image";
+
+interface SimilarProductCardProps {
+    productName: string;
+    imageUrl: string;
+    slug: string;
+    price: number;
+    buyLink: string;
+  }
+
+const SimilarProductCard:FC<SimilarProductCardProps> = (props) => {
+   const {productName, imageUrl, slug, price, buyLink,} = props
+
+  return (
+    <div className={gameCardClassNames.container}>
+        <Link href={`/products/${slug}`}>
+        <h3 className={gameCardClassNames.price}>{price} $</h3>
+
+        <Image
+            className={gameCardClassNames.image}
+            src={imageUrl}
+            alt={productName}
+            width={90}
+            height={90}
+        />
+        </Link>
+        <div className={gameCardClassNames.gameName}>{productName}
+        <Link href={buyLink}>
+        <button className={gameCardClassNames.buyButton}>Buy</button>
+        </Link>
+    </div>
+  </div>
+  )
+}
+
+export default SimilarProductCard
+
+const gameCardClassNames = {
+    container: "relative mx-auto w-[255px] h-[170px] rounded-md overflow-hidden",
+    image: "w-90 h-90 object-cover",
+    price:
+      "absolute rounded-bl-2xl top-0 right-0 bg-black bg-opacity-75 p-3 text-lg text-white z-10",
+    gameName:
+      "absolute bottom-0 left-0 right-0 py-3 px-4 text-white text-lg bg-black bg-opacity-75 flex justify-between",
+    buyButton: "bg-yellow-300 hover:bg-yellow-700 text-black px-2 font-medium "
+  };
