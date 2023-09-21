404be4da42dc9fa8c316d396d6d8c0f330ff376b
themosthigh1 <dickemmanuel@gmail.com>
2023-09-20 22:05:45 -0400

    Made the about page.

--- src/app/(pages)/about/page.tsx
+++ src/app/(pages)/about/page.tsx
@@ -4,24 +4,12 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div className=''>
+    <div>
       <PageTitle title={'About'}/>
-      <div className={pageClassNames.pageSection}>
-        <div className={pageClassNames.div1}>
-          Div 1
-        </div>
-        <div className={pageClassNames.div2}>
-          Div 2
-        </div>
-      </div>
+
     </div>
   )
 }
 
 export default page
 
-const pageClassNames = {
-  pageSection: 'text-white flex-col max-w-sm',
-  div1: 'bg-red-500 p-6 w-1/3 h-32',
-  div2: 'bg-blue-500 p-6 w-2/3',
-}
