ee1e7f16740debfbf5aff8656282ca98c1c3659c
Emmanuel Dick <dickemmanuel@gmail.com>
2023-10-17 19:22:56 -0400

    Updated Product Details Page

--- src/components/GameDetails/GameDetailsClient.tsx
+++ src/components/GameDetails/GameDetailsClient.tsx
@@ -30,12 +30,71 @@ const GameDetailsClient = (props: {
     fetchGameDetails();
   }, [slug]);
 
+  const handleDecrease = () => {
+    if (!gameDetails) return;
+    if (quantity > 0) {
+      setQuantity(quantity - 1);
+      setPrice(Number(((quantity - 1) * gameDetails.price).toFixed(2)));
+    }
+  };
+
+  const handleIncrease = () => {
+    if (!gameDetails) return;
+    if (quantity < gameDetails.quantity) {
+      setQuantity(quantity + 1);
+      setPrice(Number(((quantity + 1) * gameDetails.price).toFixed(2)));
+    }
+  };
+
+  const handleAddToCart = () => {
+    if (!gameDetails) return;
+    dispatch(addItemToCart({ ...gameDetails, quantity }));
+  };
+
   return (
     <div>
       {gameDetails && <CarouselSlider images={gameDetails.images} />}
 
       <div className={classNames.container}>
         <div className={classNames.productInfo}>
+          <div className={classNames.cartContainer}>
+            <button
+              onClick={handleDecrease}
+              className={`${classNames.button} ${
+                quantity === 0 && classNames.disabledButton
+              }`}
+              disabled={quantity === 0}
+            >
+              -
+            </button>
+            <input
+              type="text"
+              className={classNames.quantityInput}
+              value={quantity}
+              readOnly
+            />
+            {gameDetails && (
+              <button
+                onClick={handleIncrease}
+                className={`${classNames.button} ${
+                  quantity === gameDetails.quantity && classNames.disabledButton
+                }`}
+                disabled={quantity === gameDetails.quantity}
+              >
+                +
+              </button>
+            )}
+            <div className={classNames.cartPrice}>$ {price}</div>
+            <button
+              onClick={handleAddToCart}
+              className={`${classNames.button} ${
+                quantity === 0 && classNames.disabledButton
+              }`}
+              disabled={quantity === 0}
+            >
+              <FaShoppingCart />
+            </button>
+          </div>
 
           {/* Render Game Details Server */}
           {children}
@@ -49,7 +108,7 @@ export default GameDetailsClient;
 
 const classNames = {
   container:
-    "max-w-xs md:max-w-3xl mx-auto flex flex-col items-center justify-center",
+    "mt-10 max-w-xs md:max-w-3xl mx-auto flex flex-col items-center justify-center",
   carousel: "relative w-full h-64",
   previousButton:
     "absolute top-1/2 left-2 transform -translate-y-1/2 px-4 py-2 bg-gray-500 text-white rounded-l",
