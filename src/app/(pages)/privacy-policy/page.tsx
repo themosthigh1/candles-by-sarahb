98c26e259e3b4fa2ea34bd56fc9d0c3a33abdd44
themosthigh1 <dickemmanuel@gmail.com>
2023-11-05 10:08:56 -0500

    Updated privacy

--- src/app/(pages)/privacy-policy/page.tsx
+++ src/app/(pages)/privacy-policy/page.tsx
@@ -1,62 +1,102 @@
-import PageTitle from '@/components/PageTitle/PageTitle'
-import React from 'react'
-
-
+import PageTitle from "@/components/PageTitle/PageTitle";
+import React from "react";
 
 const RefundPolicy = () => {
-  
   return (
     <div>
-      <PageTitle title={'Privacy Policy'}/>
-      <div className='px-6 sm:px-12 md:px-20 lg:px-36 mx-auto text-black mb-10'>
-        <h2 className="text-xl font-semibold mb-2">PRIVACY POLICY</h2>
-
-        <p>This Privacy Policy describes how www.candlesbysarahb.com (the “Site” or “we”) collects, uses, and discloses your Personal Information when you visit or make a purchase from the Site.
+      <PageTitle title={"Privacy Policy"} />
+      <div className="px-6 sm:px-12 md:px-20 lg:px-36 mx-auto text-black mb-10">
+        <h2 className="text-xl font-medium mb-2">PRIVACY POLICY</h2>
+        <p>
+          This Privacy Policy describes how www.candlesbysarahb.com (the “Site”
+          or “we”) collects, uses, and discloses your Personal Information when
+          you visit or make a purchase from the Site.
         </p>
-
-        <h2 className="text-xl font-semibold my-2">COLLECTING PERSONAL INFORMATION</h2>
-
-        <p>When you visit the Site, we collect certain information about your device, your interaction with the Site, and information necessary to process your purchases. We may also collect additional information if you contact us for customer support. In this Privacy Policy, we refer to any information that can uniquely identify an individual (including the information below) as “Personal Information”. See the list below for more information about what Personal Information we collect and why
+        <h2 className="text-xl font-medium my-2">
+          COLLECTING PERSONAL INFORMATION
+        </h2>
+        <p>
+          When you visit the Site, we collect certain information about your
+          device, your interaction with the Site, and information necessary to
+          process your purchases. We may also collect additional information if
+          you contact us for customer support. In this Privacy Policy, we refer
+          to any information that can uniquely identify an individual (including
+          the information below) as “Personal Information”. See the list below
+          for more information about what Personal Information we collect and
+          why
         </p>
-
-        <h2 className="text-xl font-semibold my-2">DEVICE INFORMATION</h2>
-
-        <p>Examples of Personal Information collected: version of web browser, IP address, time zone, cookie information, what sites or products you view, search terms, and how you interact with the Site.
-        Purpose of collection: to load the Site accurately for you, and to perform analytics on Site usage to optimize our Site.
-        •Source of collection: Collected automatically when you access our Site using cookies, log files, web beacons, tags, or pixels.
+        <h2 className="text-xl font-medium my-2">DEVICE INFORMATION</h2>
+        <p>
+          Examples of Personal Information collected: version of web browser, IP
+          address, time zone, cookie information, what sites or products you
+          view, search terms, and how you interact with the Site. Purpose of
+          collection: to load the Site accurately for you, and to perform
+          analytics on Site usage to optimize our Site. •Source of collection:
+          Collected automatically when you access our Site using cookies, log
+          files, web beacons, tags, or pixels. Disclosure for a business
+          purpose: shared with our processor Shopify
+        </p>
+        <h2 className="text-xl font-medium my-2">ORDER INFORMATION</h2>
+        Examples of Personal Information collected: name, billing address,
+        shipping address, payment information (including credit card numbers,
+        email address, and phone number. •Purpose of collection: to provide
+        products or services to you to fulfill our contract, to process your
+        payment information, arrange for shipping, and provide you with invoices
+        and/or order confirmations, communicate with you, screen our orders for
+        potential risk or fraud, and when in line with the preferences you have
+        shared with us, provide you with information or advertising relating to
+        our products or services. Source of collection: collected from you.
         Disclosure for a business purpose: shared with our processor Shopify
-        </p>
-        <h2 className="text-xl font-semibold my-2">ORDER INFORMATION</h2>
-        Examples of Personal Information collected: name, billing address, shipping address, payment information (including credit card numbers, email address, and phone number.
-        •Purpose of collection: to provide products or services to you to fulfill our contract, to process your payment information, arrange for shipping, and provide you with invoices and/or order confirmations, communicate with you, screen our orders for potential risk or fraud, and when in line with the preferences you have shared with us, provide you with information or advertising relating to our products or services.
-        Source of collection: collected from you.
-        Disclosure for a business purpose: shared with our processor Shopify
-        Customer support information
-        Examples of Personal Information collected:
-        Purpose of collection: to provide customer support.
-        Source of collection: collected from you.
-        Disclosure for a business purpose:
-        <h2 className="text-xl font-semibold my-2">MINORS</h2>
-        The Site is not intended for individuals under the age of 18. We do not intentionally collect Personal Information from children. If you are the parent or guardian and believe your child has provided us with Personal Information, please contact us at the address below to request deletion.
-        <h2 className="text-xl font-semibold my-2">SHARING PERSONAL INFORMATION</h2>
-        We share your Personal Information with service providers to help us provide our services and fulfill our contracts with you, as described above. For example:
-        •We may share your Personal Information to comply with applicable laws and regulations, to respond to a subpoena, search warrant or other lawful request for information we receive, or to otherwise protect our rights.
-
-        <h2 className="text-xl font-semibold my-2">USING PERSONAL INFORMATION</h2>
-        We use your personal Information to provide our services to you, which includes: offering products for sale, processing payments, shipping and fulfillment of your order, and keeping you up to date on new products, services, and offers.
-
-        <h2 className="text-xl font-semibold my-2">RETENTION</h2>
-        When you place an order through the Site, we will retain your Personal Information for our records unless and until you ask us to erase this information. For more information on your right of erasure, please see the ‘Your rights’ section below.
-
-        <h2 className="text-xl font-semibold my-2">COOKIES</h2>
-        A cookie is a small amount of information that’s downloaded to your computer or device when you visit our Site. We use a number of different cookies, including functional, performance, advertising, and social media or content cookies. Cookies make your browsing experience better by allowing the website to remember your actions and preferences (such as login and region selection). This means you don’t have to re-enter this information each time you return to the site or browse from one page to another. Cookies also provide information on how people use the website, for instance whether it’s their first time visiting or if they are a frequent visitor.
-        <h2 className="text-xl font-semibold my-2">DO NOT TRACK</h2>
-        Please note that because there is no consistent industry understanding of how to respond to “Do Not Track” signals, we do not alter our data collection and usage practices when we detect such a signal from your browser.
-        <h2 className="text-xl font-semibold my-2">CHANGES</h2>
-        We may update this Privacy Policy from time to time in order to reflect, for example, changes to our practices or for other operational, legal, or regulatory reasons.
+        Customer support information Examples of Personal Information collected:
+        Purpose of collection: to provide customer support. Source of
+        collection: collected from you. Disclosure for a business purpose:
+        <h2 className="text-xl font-medium my-2">MINORS</h2>
+        The Site is not intended for individuals under the age of 18. We do not
+        intentionally collect Personal Information from children. If you are the
+        parent or guardian and believe your child has provided us with Personal
+        Information, please contact us at the address below to request deletion.
+        <h2 className="text-xl font-medium my-2">
+          SHARING PERSONAL INFORMATION
+        </h2>
+        We share your Personal Information with service providers to help us
+        provide our services and fulfill our contracts with you, as described
+        above. For example: •We may share your Personal Information to comply
+        with applicable laws and regulations, to respond to a subpoena, search
+        warrant or other lawful request for information we receive, or to
+        otherwise protect our rights.
+        <h2 className="text-xl font-medium my-2">USING PERSONAL INFORMATION</h2>
+        We use your personal Information to provide our services to you, which
+        includes: offering products for sale, processing payments, shipping and
+        fulfillment of your order, and keeping you up to date on new products,
+        services, and offers.
+        <h2 className="text-xl font-medium my-2">RETENTION</h2>
+        When you place an order through the Site, we will retain your Personal
+        Information for our records unless and until you ask us to erase this
+        information. For more information on your right of erasure, please see
+        the ‘Your rights’ section below.
+        <h2 className="text-xl font-medium my-2">COOKIES</h2>A cookie is a small
+        amount of information that’s downloaded to your computer or device when
+        you visit our Site. We use a number of different cookies, including
+        functional, performance, advertising, and social media or content
+        cookies. Cookies make your browsing experience better by allowing the
+        website to remember your actions and preferences (such as login and
+        region selection). This means you don’t have to re-enter this
+        information each time you return to the site or browse from one page to
+        another. Cookies also provide information on how people use the website,
+        for instance whether it’s their first time visiting or if they are a
+        frequent visitor.
+        <h2 className="text-xl font-medium my-2">DO NOT TRACK</h2>
+        Please note that because there is no consistent industry understanding
+        of how to respond to “Do Not Track” signals, we do not alter our data
+        collection and usage practices when we detect such a signal from your
+        browser.
+        <h2 className="text-xl font-medium my-2">CHANGES</h2>
+        We may update this Privacy Policy from time to time in order to reflect,
+        for example, changes to our practices or for other operational, legal,
+        or regulatory reasons.
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default RefundPolicy
+export default RefundPolicy;
