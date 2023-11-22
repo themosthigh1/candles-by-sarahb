a31ce1902e8c7a6a096465a78ab79dbfd939da23
themosthigh1 <dickemmanuel@gmail.com>
2023-11-22 12:59:12 -0500

    Added Samples Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default async function Home() {
   const games = await getGames();
   const isTrendingGames = games?.filter((game) => game.isTrending);
   const isFeaturedGame = games?.find((game) => game.isFeatured);
-  const recentGames = await getRecentGames()
+  const recentGames = await getRecentGames();
 
   return (
     <>
@@ -23,7 +23,7 @@ export default async function Home() {
       <section className={sectionClassNames.section}>
         <div className={sectionClassNames.trending}>
           <h2 className={sectionClassNames.trendingTitle}>
-          The Redolence Collection
+            The Redolence Collection
           </h2>
         </div>
 
@@ -35,7 +35,7 @@ export default async function Home() {
               imageUrl={game.images[0].url}
               slug={game.slug.current}
               price={game.price}
-              buyLink={game.buyLink} 
+              buyLink={game.buyLink}
             />
           ))}
         </div>
@@ -58,49 +58,6 @@ const sectionClassNames = {
   featuredContent: "mx-auto max-w-screen-xl",
 };
 
-// const games = [
-//   {
-//     id: 1,
-//     price: 12,
-//     name: "Call of Duty: Modern Warfare",
-//     slug: "call-of-duty",
-//     image:
-//       "https://images.unsplash.com/photo-1602673221577-0b56d7ce446b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8Q2FsbCUyMG9mJTIwRHV0eXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60",
-//   },
-//   {
-//     id: 2,
-//     price: 14,
-//     name: "Assassin's Creed Valhalla",
-//     slug: "assassin-creed",
-//     image:
-//       "https://images.unsplash.com/photo-1586325194227-7625ed95172b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8QXNzYXNzaW4ncyUyMENyZWVkfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
-//   },
-//   {
-//     id: 3,
-//     price: 42,
-//     name: "FIFA 23",
-//     slug: "fifa-23",
-//     image:
-//       "https://images.unsplash.com/photo-1493711662062-fa541adb3fc8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cGxheSUyMHN0YXRpb24lMjBmaWZhfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
-//   },
-//   {
-//     id: 4,
-//     price: 27,
-//     name: "The Legend of Zelda: Breath of the Wild",
-//     slug: "the-legend-of-zelda",
-//     image:
-//       "https://images.unsplash.com/photo-1500856056008-859079534e9e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bGVnZW5kfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
-//   },
-// ];
-
-// const featuredGame = {
-//   name: "Eternal Domination",
-//   description:
-//     "Immerse yourself in a vast fantasy realm where epic battles and strategic conquests await. In 'Eternal Domination,' you'll lead armies, forge alliances, and build your empire from scratch. Command powerful heroes, employ cunning tactics, and unleash your might on the battlefield. Will you rise as the supreme ruler or fall beneath the weight of your ambitions? Join the fray and claim your destiny in this thrilling strategy game.",
-//   slug: "eternal-domination",
-//   image: "/images/trending.jpeg",
-// };
-
 const featuredClassNames = {
   gameName: "font-bold text-2xl md:text-3xl lg:text-4xl mb-4 md:mb-8",
   gameDetails: "max-w-screen-md text-sm mb-8 md:mb-12",
@@ -122,27 +79,3 @@ const recentGamesClasses = {
   heading: "text-3xl lg:text-4xl font-bold mb-3",
   subHeading: "text-gray-400 max-w-xl mx-auto lg:text-lg",
 };
-
-// const categories = [
-//   {
-//     id: 1,
-//     name: "Action",
-//     slug: "action",
-//     image:
-//       "https://images.unsplash.com/photo-1552072092-7f9b8d63efcb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8ZmlnaHR8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
-//   },
-//   {
-//     id: 2,
-//     name: "Adventure",
-//     slug: "adventure",
-//     image:
-//       "https://images.unsplash.com/photo-1536751048178-14106afab4f9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8cmFjaW5nfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
-//   },
-//   {
-//     id: 3,
-//     name: "Sports",
-//     slug: "sports",
-//     image:
-//       "https://images.unsplash.com/photo-1579952363873-27f3bade9f55?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8c3BvcnRzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
-//   },
-// ];
