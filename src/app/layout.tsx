8b29b73a4904283a932001b96d4a0d432ad776f7
themosthigh1 <dickemmanuel@gmail.com>
2023-11-26 14:19:25 -0500

    removed omnisend

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -46,16 +46,6 @@ export default function RootLayout({
             <Footer />
           </NextAuthProvider>
         </Providers>
-        <Script id="omnisend-script">
-          {`window.omnisend = window.omnisend || [];
-        omnisend.push(["accountID", "655f796c692f14abf0a78586"]);
-        omnisend.push(["track", "$pageViewed"]);
-        !function(){var e=document.createElement("script");
-        e.type="text/javascript",e.async=!0,
-        e.src="https://omnisnippet1.com/inshop/launcher-v2.js";
-        var t=document.getElementsByTagName("script")[0];
-        t.parentNode.insertBefore(e,t)}()`}
-        </Script>
       </body>
     </html>
   );
